0bbbbf9a3fd95d77f4fb661e3102334803bdbfda
kjai9nxt <khushi.jain@nxtwave.co.in>
2025-07-24 11:44:39 +0530

    added functionalities to home and header

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-
+import { Link } from 'react-router-dom';
 import { FaBook, FaShoppingBag } from 'react-icons/fa';
 
 import "./index.css"
@@ -8,19 +8,26 @@ class Header extends Component {
       return (
           <nav className="header-container">
              <div className="header-content-container">
+                <Link to = "/" className = "nav-link">
                 <div className="logo-container">
                    <div className="logo">B</div>
                    BOOKSTORE
                 </div>
+                    </Link>"
                 <ul className="nav-items-container">
+                 <Link to = "/books" className = "nav-link">
                   <li className="icon-container">
                       <FaBook className="icon" />
                       <span className="icon-name">Book List</span>
                   </li>
+                  </Link>
+
+                    <Link to = "/cart" className = "nav-link">
                   <li className="icon-container">
                       <FaShoppingBag className="icon" />
                       <span className="icon-name">Cart</span>
                   </li>
+                    </Link>
               </ul>
           </div>
        </nav>
